652320164deb97669bb3bbc46165ebeb36a739b0
MauricioMunguia10 <mauriciomunguiagarcia@gmail.com>
2022-06-10 17:09:04 -0500

    add: Condicionales-Ejercicio4

---
+++ Condicionales/ejercicio4.py
@@ -0,0 +1,7 @@
+#Escribir un programa que pida al usuario un número entero y muestre por pantalla si es par o impar.
+print("Par o Impar")
+num = int(input("Ingrese un numero entero:"))
+if num%2:
+    print("El numero es impar.")
+else:
+    print("El nuemro es par.")
